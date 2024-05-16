6d3d37365f4b8373f23edf1004a78021545d3926
Sahil <sahil2215ali@gmail.com>
2024-05-16 23:09:26 +0530

    refactors

---
+++ src/index.js
@@ -0,0 +1,88 @@
+const express  = require('express');
+const app = express();
+const https = require('https');
+const path = require('path');
+const hbs = require('hbs');
+// const { collection, weatherCollection } = require('./mongodb'); 
+const collection = require('./mongodb'); 
+
+const tempelatePath = path.join(__dirname , '../tempelates')
+app.use(express.json())
+app.set('view engine', 'hbs')
+app.set('views', tempelatePath)
+app.use(express.urlencoded({exended:false}))
+
+app.get('/login', (req , res) => {
+  res.render("login");
+})
+
+app.get('/', (req , res) => {
+  res.render("signup");
+})
+
+
+app.post("/signup", async(req , res) => {
+   const data = {
+    name: req.body.name,
+    password: req.body.password
+   }
+
+    await collection.insertMany([data])
+    res.render("home")
+
+})
+
+app.post("/login", async(req , res) => {
+ try{
+  const check = await collection.findOne({name: req.body.name})
+  if(check.password === req.body.password){
+    res.render(homeData)
+  }
+  else{
+    res.send("wrong Password")
+  }
+
+ }
+ catch{
+  res.send("wrong details")
+ }
+
+ }
+)
+
+app.post('/',  (req, res) => {
+  const query = req.body.cityName;
+  const apiKey = '4d8bf00bb34af0cc25e6e827592ce03c';
+  const url = 'https://api.openweathermap.org/data/2.5/weather?q=' + query + '&appid=' + apiKey + '&units=metric';
+
+  https.get(url, (response) => {
+      let data = '';
+      
+
+      response.on('data', (chunk) => {
+          data += chunk;
+      });
+
+      response.on('end', async () => {
+          const weatherData = JSON.parse(data);
+          const temp = weatherData.main.temp;
+          const description = weatherData.weather[0].description;
+          // Sending  the response to the client
+         res.send("<h1>The temperature in " + query + " is " + temp + " degree celsius </h1>" +
+              "<p> Feels like: " + description + "</p>");
+
+              const newData = {
+                cityName: query,
+                temperature: temp,
+                desc: description
+            };
+
+            await collection.insertMany([newData])
+      });
+  });
+});
+
+
+app.listen(3000 , () => {
+  console.log(`port connected at http://localhost:3000`);
+})
---
+++ src/mongodb.js
@@ -0,0 +1,41 @@
+const mongoose=require("mongoose")
+
+mongoose.connect("mongodb://localhost:27017/WeatherData")
+.then(()=>{
+    console.log('mongoose connected');
+})
+.catch((e)=>{
+    console.log('failed');
+})
+
+const LogInSchema=new mongoose.Schema({
+    name:{
+        type:String,
+        requird: [true, "Please provide name"]
+
+    },
+    password:{
+        type:String,
+        requird: [true, "Please provide password"],
+        unique: true 
+    },
+    weatherSchema: [{ cityName: String, temperature: Number, desc: String}]
+});
+// const weatherSchema = new mongoose.Schema({
+//   cityName: {
+//       type: String
+//   },
+//   temperature: {
+//       type: Number
+//   },
+//   desc: {
+//       type: String
+//   },
+
+// });
+
+const collection = mongoose.model('Collection1', LogInSchema);
+// const weatherCollection = mongoose.model('WeatherCollection1', weatherSchema);
+// module.exports = {collection  , weatherCollection}
+
+ module.exports = collection;
